44e375edd6f61b7076a302ad3699a2c9e94d0d55
qa <jerome.galan@luos.io>
2022-09-10 15:16:10 +0200

    debug n5 suite

--- NUCLEO-F072RB/src/stm32f0xx_it.c
+++ NUCLEO-F072RB/src/stm32f0xx_it.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    stm32f0xx_it.c
-  * @brief   Interrupt Service Routines.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    stm32f0xx_it.c
+ * @brief   Interrupt Service Routines.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -27,15 +27,15 @@
 /*           Cortex-M0 Processor Interruption and Exception Handlers          */
 /******************************************************************************/
 /**
-  * @brief This function handles Non maskable interrupt.
-  */
+ * @brief This function handles Non maskable interrupt.
+ */
 void NMI_Handler(void)
 {
 }
 
 /**
-  * @brief This function handles Hard fault interrupt.
-  */
+ * @brief This function handles Hard fault interrupt.
+ */
 void HardFault_Handler(void)
 {
     while (1)
@@ -44,22 +44,22 @@ void HardFault_Handler(void)
 }
 
 /**
-  * @brief This function handles System service call via SWI instruction.
-  */
+ * @brief This function handles System service call via SWI instruction.
+ */
 void SVC_Handler(void)
 {
 }
 
 /**
-  * @brief This function handles Pendable request for system service.
-  */
+ * @brief This function handles Pendable request for system service.
+ */
 void PendSV_Handler(void)
 {
 }
 
 /**
-  * @brief This function handles System tick timer.
-  */
+ * @brief This function handles System tick timer.
+ */
 void SysTick_Handler(void)
 {
     HAL_IncTick();
@@ -73,17 +73,13 @@ void SysTick_Handler(void)
 /******************************************************************************/
 void EXTI4_15_IRQHandler(void)
 {
-#ifdef PTP_DISABLED
-    return;
-#endif
-
+#ifndef PTP_DISABLED
     uint32_t pending = EXTI->PR;
     if (pending & PTPA_PIN)
     {
         HAL_GPIO_EXTI_IRQHandler(PTPA_PIN);
     }
 
-#if defined(PTP_CONFIG_AB) || defined(PTP_CONFIG_AC) || defined(PTP_CONFIG_AD) || defined(PTP_CONFIG_BC) || defined(PTP_CONFIG_BD) || defined(PTP_CONFIG_CD)
     if (pending & PTPB_PIN)
     {
         HAL_GPIO_EXTI_IRQHandler(PTPB_PIN);
